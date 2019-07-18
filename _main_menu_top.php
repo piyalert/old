724b0f3c5c2d1c6c9e5f3684c06f3be529ec8682
poomin <poominjaichoung@gmail.com>
2019-07-18 21:55:23 +0700

    update

--- _main_menu_top.php
+++ _main_menu_top.php
@@ -2,21 +2,20 @@
 <header class="header trans_300">
 
     <!-- Main Navigation -->
-
-    <div class="main_nav_container bg-warning ">
+    <div class="main_nav_container bg-warning">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-right ">
+                <div class="col-lg-12 text-right">
                     <div class="logo_container">
                         <a href="index.php">การดูแล<span>ผู้สูงอายุ</span></a>
                     </div>
                     <nav class="navbar">
                         <ul class="navbar_menu">
-                            <li><a href="barthel.php">Barthel ADL index</a></li>
-                            <li><a href="chula.php">Chula ADL Index</a></li>
-                            <li><a href="activityphoto.php">ภาพกิจกรรม</a></li>
-                            <li><a href="takecare.php">การดูแลผู้สูงอายุ</a></li>
-                            <li><a href="contact.php">contact</a></li>
+                            <li ><a href="barthel.php">Barthel ADL index</a></li>
+                            <li ><a href="chula.php">Chula ADL Index</a></li>
+                            <li ><a href="activityphoto.php">ภาพกิจกรรม</a></li>
+                            <li ><a href="takecare.php">การดูแลผู้สูงอายุ</a></li>
+                            <li ><a href="contact.php">contact</a></li>
                         </ul>
                         <div class="hamburger_container">
                             <i class="fa fa-bars" aria-hidden="true"></i>
