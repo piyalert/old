a6444fc41e6b91ddc2e6af386137924c91fc9533
poomin <poominjaichoung@gmail.com>
2019-10-11 09:36:16 +0700

    update

--- barthel-test.php
+++ barthel-test.php
@@ -3,7 +3,8 @@ session_start();
 
 $SET_EQ = 1;
 $CHOICES = [];
-$TEST = [];
+$TESTS = [];
+$TEST = ['test_top'=>'','test_title'=>''];
 //include('controller/barthelTestController.php');
 include __DIR__.'/controller/barthelTestController.php';
 ?>
