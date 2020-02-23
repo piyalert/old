5ef2ae87211ed3117be861b1f873d7f60efd16f9
tnonkaew <tnonkaew@gmail.com>
2020-02-23 10:43:01 +0700

    tt

--- _footer.php
+++ _footer.php
@@ -9,8 +9,8 @@
 <div class="footer">
     <div class="text-center p-2">
         <span class="d-block text-white font-weight-bold mb-3"> @Happy Life Healthy Aging </span>
-        <img src="./img/logo-nurse.jpg" class="img-fluid ml-3" alt="image logo">
-        <img src="./img/logo-ubu.jpg" class="img-fluid ml-3" alt="image logo">
-        <img src="./img/logo-facebook.jpg" class="img-fluid ml-3" alt="image logo">
+        <img src="./img/logo-ubu.png" style="max-height: 70px;"class="img-fluid ml-3" alt="image logo">
+        <img src="./img/logo-nurse.png" style="max-height: 70px;" class="img-fluid ml-3" alt="image logo">
+        <img src="./img/logo-facebook.png" style="max-height: 70px;"class="img-fluid ml-3" alt="image logo">
     </div>
 </div>
